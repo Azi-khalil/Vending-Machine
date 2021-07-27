70179844d85c46af1be976bc19b4e3e800a34280
Aslam Khalil <83498247+Azi-khalil@users.noreply.github.com>
2021-07-27 23:52:27 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,143 @@
-import logo from './logo.svg';
+import data from './Data'
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [total, setTotal] = useState(0)
+  const [item, setItem] = useState(0)
+  const [insert, setInsert] = useState(0)
+  const [change, setChange] = useState(0)
+  const [message, setMessage] = useState('')
+
+
+
+const changeHandler = (e) => {
+   setMessage('')
+   setItem(e.target.value)
+}
+
+const moneyHandler = (num) => {
+  setInsert(insert + num)
+  setMessage('')
+}
+const clearMoney = () => {
+  setTotal(0)
+  setInsert(0)
+  setChange(0)
+  setMessage('')
+}
+ 
+
+
+function enterItem()  {
+ data.forEach((snack) => {
+  if (snack.id === parseInt(item) && snack.available === true ){
+    setTotal(total + snack.price)
+    setMessage('')
+    setMessage('Insert Money Please')
+  }
+
+  if(snack.available === false){
+   setMessage('This Snack is Unavailable')
+   setTotal(0)
+  } 
+    })  
+    
+}
+
+
+function payMoney() {
+  if (insert === total){
+    setTimeout(() => {
+      clearMoney()
+      setMessage('Enjoy Your Snack!')
+    }, 2000);
+  }else if (insert > total){
+    setChange(insert - total)
+    setMessage('Please Take your Change')
+    setTimeout(() => {
+        clearMoney()
+        setMessage('Enjoy Your Snack!')
+        setItem('')
+      }, 2000);
+  } else if (insert < total) {
+    setMessage('Please Complete the Sum')
+  }
+  setTimeout(() => {
+    setItem(0)
+  }, 1000)
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='super'>
+      <div className="con1">
+        <span>{message}</span>
+        <h3>Total Price is ${total}</h3>
+        <h3>Money Inserted ${insert}</h3>
+        <h3>Change Availabe ${change}</h3>
+        <input 
+        type='number'
+        onChange ={changeHandler}
+        placeholder='Insert Number of Snack Here'/>
+        <button onClick={() => enterItem()}>Enter</button>  
+      </div>
+       
+      <div className='con2'>
+      <h6>Insert Money (only in USD)</h6>
+
+        <button onClick={() => moneyHandler(20)}>$20</button>
+        <button onClick={() => moneyHandler(50)}>$50</button>
+        <button onClick={() => moneyHandler(1)}>$1</button>
+        <button onClick={() => moneyHandler(0.20)}>20c</button>
+        <button onClick={() => moneyHandler(0.10)}>10c</button>
+        <button onClick={() => clearMoney()}>Clear All</button>
+        <button onClick={() => payMoney()}>Pay</button>
+
+      </div>
+
+      <div className='con3'>
+      <table>
+       <tbody>
+          <tr>
+            <td>#1 <br /> Snickers</td>
+            <td>#2 <br /> Mars</td>
+            <td>#3 <br /> Milky</td>
+            <td>#4 <br /> M&M</td>
+            <td>#5 <br /> Layz</td>
+          </tr>
+          <tr>
+            <td>#6 <br /> Crackers</td>
+            <td>#7 <br /> Walkers</td>
+            <td>#8 <br /> Minnies</td>
+            <td>#9 <br /> Lion</td>
+            <td>#10 <br /> Ruby</td>
+          </tr>
+          <tr>
+            <td>#11 <br /> Mixups</td>
+            <td>#12 <br />Walkers</td>
+            <td>#13 <br />Max</td>
+            <td>#14 <br />Pops</td>
+            <td>#15 <br />Munch</td>
+          </tr>
+          <tr>
+            <td>#16 <br />Icons</td>
+            <td>#17 <br />Snaps</td>
+            <td>#18 <br />Waffles</td>
+            <td>#19 <br />Chops</td>
+            <td>#20 <br />Weelies</td>
+          </tr>
+          <tr>
+            <td>#21 <br />Snax</td>
+            <td>#22 <br />Meez</td>
+            <td>#23 <br />Lees</td>
+            <td>#24 <br />Coon</td>
+            <td>#25 <br />Jeager</td>
+          </tr>
+        </tbody>
+    </table>
+      </div>
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,160 @@
+const data = 
+ [
+     {
+        id: 1,
+        name: 'Snickers',
+        price: 2.50,
+        available: false
+      
+    }, {
+        id: 2,
+        name: 'Mars',
+        price: 2.00,
+        available: true,
+      }
+    , {
+        id: 3,
+        name: 'Milky',
+        price: 3.00,
+        available: true
+    }, {
+        id: 4,
+        name: 'M&M',
+        price: 4.00,
+        available: true
+    }, 
+    {
+        id: 5,
+    name: 'Layz',
+    price: 5.50,
+    available: false
+    },
+    {
+        id: 6,
+    name: 'Crackers',
+    price: 1.50,
+    available: true
+    },
+    {
+        id: 7,
+    name: 'Walkers',
+    price: 2.50,
+    available: false
+    },
+    {
+        id: 8,
+    name: 'Minnies',
+    price: 2.40,
+    available: false
+    },
+    {
+        id: 9,
+    name: 'Lion',
+    price: 4.50,
+    available: true
+    },
+    {
+        id: 10,
+    name: 'Ruby',
+    price: 40,
+    available: true
+    },
+    {
+        id: 11,
+    name: 'Mixups',
+    price: 50,
+    available: true
+    },
+    {
+        id: 12,
+    name: 'Walkers',
+    price: 7.50,
+    available: true
+    },
+    {
+        id: 13,
+    name: 'Max',
+    price: 8,
+    available: true
+    },
+    {
+        id: 14,
+    name: 'Pops',
+    price: 30,
+    available: true
+    },
+    {
+        id: 15,
+    name: 'Monster Munch',
+    price: 10,
+    available: true
+    },
+    
+    {
+        id: 16,
+    name: 'Icons',
+    price: 11.50,
+    available: true
+    },
+    {
+        id: 17,
+    name: 'Snaps',
+    price: 12,
+    available: true
+    },
+    {
+        id: 18,
+    name: 'Waffles',
+    price: 15.50,
+    available: true
+    },
+    {
+        id: 19,
+    name: 'Chops',
+    price: 50,
+    available: true
+    },
+    {
+        id: 20,
+    name: 'Weelies',
+    price: 5,
+    available: true
+    },
+    {
+        id: 21,
+    name: 'Snax',
+    price: 15.50,
+    available: true
+    },
+    {
+        id: 22,
+    name: 'Meez',
+    price: 13,
+    available: true
+    },
+    {
+        id: 23,
+    name: 'Lees',
+    price: 20,
+    available: true
+    },
+    {
+        id: 24,
+    name: 'Coon',
+    price: 18,
+    available: true
+    },
+    {
+        id: 25,
+    name: 'Jeager',
+    price: 80,
+    available: true
+    },
+
+]
+  
+
+export default data;
+
+
+
